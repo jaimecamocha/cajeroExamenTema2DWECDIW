3a2f04ce95b82fd8fce757f61896eb8bfea491ed
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-30 09:33:35 +0100

    Creamos la estructura HTML y JavaScript

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+const pin = "123";
+let intentosPin = 3;
+const saldo = parseFloat(document.getElementById("txtSaldo"));
+
+
+// Mostrar saldo
+function mostrarSaldo(){
+    
+}
+
+// Depositar dinero
+function depositarDinero(){
+
+}
+
+// Retirar dinero
+function retirarDinero(){
+
+}
+
+// Transferir dinero
+function transferirDinero(){
+
+}
+
+// Cambiar contraseña
+function cambiarContrasena(){
+
+}
+
+// Iniciar sesión
+function iniciarSesion(){
+
+}
+
+// Cerrar sesión
+function cerrarSesion(){
+
+}
