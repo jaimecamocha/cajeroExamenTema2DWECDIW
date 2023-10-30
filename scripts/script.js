905dd8b0f46efe189ea81718aa219cd0bcfadeee
Jaime Fernandez <jaimefernandezcalvo@alumnos.tuniversformacion.es>
2023-10-30 11:56:33 +0100

    Acabamos el resto de funciones

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,92 @@
 let saldo = 1000;
-const PIN_CORRECTO = "123";
+let PIN_CORRECTO = "123";
 let intentosPin = 3;
+let intentosRestantes = intentosPin;
 
 // Mostrar saldo
-function mostrarSaldo(){
-    const saldoMos = parseFloat(document.getElementById("cantSaldo"));
-    alert(saldoMos);
-    txtsaldo.textContent(`Su saldo es de: ${saldoMos.toFixed(2)} euros`);
+function mostrarSaldo() {
+    const cantSaldo = document.getElementById("cantSaldo");
+    cantSaldo.textContent = `Su saldo actual es: ${saldo.toFixed(2)} €`;
 }
-
 // Depositar dinero
 function depositarDinero(){
-    const deposito = parseFloat(document.getElementById("inputCantidad"));
+    const deposito = document.getElementById("inputCantidad");
+    const depositoCant = parseFloat(deposito.value);
 
-    if(deposito <= 0){
+    if(isNaN(depositoCant) || depositoCant <= 0){
         alert("No es posible depositar ese dinero")
     } else{
-        saldo = saldo + deposito;
+        saldo = saldo + depositoCant;
         mostrarSaldo();
-        alert(`Se han depositado ${deposito.toFixed(2)} euros`);
+        alert(`Se han depositado ${depositoCant.toFixed(2)} euros`);
     }
 } 
 
 // Retirar dinero
 function retirarDinero(){
-    const retiro = parseFloat(document.getElementById("inputCantidad"));
+    const retiro = document.getElementById("inputCantidad");
+    const retiroCant = parseFloat(retiro.value);
 
-    if(retiro <= 0 || retiro > saldo){
+    if(isNaN(retiroCant) || retiroCant <= 0 || retiroCant > saldo){
         alert("No es posible retirar ese dinero")       
     } else{
-        saldo = saldo - retiro;
+        saldo = saldo - retiroCant;
         mostrarSaldo();
-        alert(`Se han retirado ${retiro.toFixed(2)} euros`);       
+        alert(`Se han retirado ${retiroCant.toFixed(2)} euros`);       
     }
 }
 
 // Transferir dinero
 function transferirDinero(){
-    const transfer = parseFloat(document.getElementById("inputCantidad"));
+    const transfer = document.getElementById("inputCantidad");
+    const transferCant = parseFloat(transfer.value);
 
-    if(transfer > saldo){
+    if(isNaN(transfer) || transferCant > saldo){
         alert("No es posible transferir esa cantidad de dinero");
     } else{
-        saldo = saldo - transfer;
+        const numCuenta = prompt("Introduzca el número de cuenta: ");
+        saldo = saldo - transferCant;
         mostrarSaldo();
-        alert(`Se han transferido ${transfer.toFixed(2)} euros`)
+        alert(`Se han transferido ${transferCant.toFixed(2)} euros a ${numCuenta}`);
     }
 } 
  
 // Cambiar contraseña
 function cambiarContrasena(){
+    const contraBien = prompt("Introduzca su contraseña actual: ");
 
+    if(PIN_CORRECTO == contraBien){
+        const nuevaContra = prompt("Introduzca la nueva contraseña: ");
+        PIN_CORRECTO = nuevaContra;     
+    }
 }
 
 // Iniciar sesión
-function iniciarSesion(){
+function iniciarSesion() {
     const pinIntroducido = document.getElementById("pinIntroducido");
 
-    for(const intento = 0; intento < intentosPin; intento++){
-        if(PIN_CORRECTO == pinIntroducido){
-            alert("INICIO DE SESIÓN CORRECTO");
-            break;
-        } else{
-    
+    if (pinIntroducido.value === PIN_CORRECTO) {
+        alert("INICIO DE SESIÓN EXITOSO");
+        pinIntroducido.value = "";
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("cajero").style.display = "block";
+        mostrarSaldo();
+    } else {
+        intentosRestantes--;
+
+        if (intentosRestantes > 0) {
+            alert(`PIN incorrecto. Te quedan ${intentosRestantes} intentos.`);
+        } else {
+            alert("PIN INCORRECTO. CAJERO BLOQUEADO");
+            document.getElementById("pinIntroducido").disabled = true;
         }
+
+        pinIntroducido.value = "";
     }
 }
 
 // Cerrar sesión
 function cerrarSesion(){
-    
+    document.getElementById("index.html").style.display = close;
+    document.getElementById("salir.html").style.display = open;
 }
